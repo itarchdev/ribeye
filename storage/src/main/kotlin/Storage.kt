54781d070a47f6bedc5e5aac4d8fe2bf2c2cd1f9
Tepex <tepex@mail.ru>
2026-01-09 12:23:50 +0300

    wip(storage): switch to refator slot subdomain

--- storage/src/main/kotlin/Storage.kt
+++ storage/src/main/kotlin/Storage.kt
@@ -1,5 +1,6 @@
 package ru.it_arch.tools.samples.ribeye.storage
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import ru.it_arch.tools.samples.ribeye.ResourceRepository
@@ -11,7 +12,9 @@ import ru.it_arch.tools.samples.ribeye.storage.impl.toRosemary
 import ru.it_arch.tools.samples.ribeye.storage.impl.toSauceIngredients
 import ru.it_arch.tools.samples.ribeye.storage.impl.format
 import ru.it_arch.tools.samples.ribeye.storage.slot.Slot
+import kotlin.math.pow
 import kotlin.reflect.KClass
+import kotlin.time.Duration.Companion.milliseconds
 
 public class Storage(
     private val meatSlotCapacity: Quantity.Piece,
@@ -38,8 +41,40 @@ public class Storage(
         type: KClass<out T>,
         requestQuantity: Q
     ): Result<T> = when (type) {
+        /* Переиспользуемый слот, который может быть в данный момент занят другим потоком.
+        Делаются 3 попытки захвата с проверкой версии и экспоненциальной задержкой между попытками */
         Resource.Meat::class -> getSlot<Slot.Pack>(type)?.let { slot ->
-            slot.get(requestQuantity.toString()).map { it.second.toMeat() as T }
+            repeat(ATTEMPTS) { attempt ->
+                // 1. Capture the version BEFORE starting any work
+                val startVersion = slot.currentVersion
+
+                try {
+                    slot.get(requestQuantity.toString()).map { (version, res) ->
+
+                        // 3. OPTIMISTIC CHECK:
+                        // In this case, since the manager itself increments the version
+                        // during the atomic mutex block, 'newVersion' must be exactly
+                        // 'startVersion + 1' if no one else touched it.
+                        if (version == startVersion + 1) {
+                            res.toMeat() as T
+                        } else {
+                            throw StaleSlotVersion("Concurrent modification detected")
+                        }
+
+
+                    }.onFailure { return@let notFound(type) }
+
+                } catch (_: StaleSlotVersion) {
+                    if (attempt < (ATTEMPTS - 1)) delay(attempt.nextDelay())
+                }
+
+                getSlot<Slot.Pack>(type)?.let { slot ->
+                    slot.get(requestQuantity.toString()).map { it.second.toMeat() as T }
+                } ?: notFound(type)
+            }
+
+            notFound(type)
+
         } ?: notFound(type)
 
         Resource.Grill::class -> getSlot<Slot.Weight>(type)?.let { slot ->
@@ -92,4 +127,11 @@ public class Storage(
 
             else -> error("Unknown resource type: ${type.simpleName}")
         }
+
+    public companion object {
+        private const val ATTEMPTS = 3
+
+        /** Расчет экспонентциальной задержки */
+        private fun Int.nextDelay() = 100.milliseconds * 2.0.pow(this)
+    }
 }
