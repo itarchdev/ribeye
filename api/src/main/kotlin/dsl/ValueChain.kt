8690a9500857fc39ca44efd4cb8b3af215b5a5ec
Tepex <tepex@mail.ru>
2026-01-23 15:17:46 +0300

    fix(api): make ValueChain Comparable

--- api/src/main/kotlin/dsl/ValueChain.kt
+++ api/src/main/kotlin/dsl/ValueChain.kt
@@ -5,8 +5,20 @@ import ru.it_arch.k3dm.ValueObject
 /**
  * Добавленная стоимость в минимальных единицах учета — копейки, центы, сатоши, внутренняя валюта.
  * */
-public interface ValueChain : ValueObject.Value<Long> {
+public interface ValueChain : ValueObject.Value<Long>, Comparable<ValueChain> {
     override fun validate() {
         require(boxed >= 0) { "Value Chain must be >= 0" }
     }
+
+    override fun compareTo(other: ValueChain): Int =
+        boxed.compareTo(other.boxed)
+
+    public operator fun plus(other: ValueChain): ValueChain =
+        apply(boxed + other.boxed)
+
+    public operator fun minus(other: ValueChain): ValueChain =
+        apply(boxed - other.boxed)
+
+    public operator fun times(other: ValueChain): ValueChain =
+        apply(boxed * other.boxed)
 }
